e9c4b4fcdd4088b0378afa6cc44ac0ed4b153a9b
iman memarpour <iman.memarpour@gmail.com>
2018-03-13 22:36:37 -0400

    Initial

---
+++ AngularBoilerPlate/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AngularBoilerPlate.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
